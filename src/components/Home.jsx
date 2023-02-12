11b31289266f4942c5c64113a5c4eede3b1974f6
SaumarDev <saumar_1@hotmail.com>
2023-02-12 00:35:01 -0600

    feat: adding styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,17 +11,17 @@ const Home = () => {
   console.log(data);
 
   return (
-    <div className="container-fluid mt-5">
-      <div className="row text-start">
+    <div className="container mt-5">
+      <div className="row text-start g-0" >
         {data.map((data, index) => (
-          <div className="col-6">
-            <div className="card text-bg-white" key={index}>
+          <div className="col-md-6 col-sm-12">
+            <div className="card rounded-0" key={index}>
               <img
-                src="https://images.unsplash.com/photo-1675019335923-05c6ddce8546?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE1NTg1Ng&ixlib=rb-4.0.3&q=80&w=1080"
-                className="card-img background-img"
-                alt="..."
+                src="https://images.unsplash.com/photo-1675273673229-6b80c1dc33bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE4MzU3Ng&ixlib=rb-4.0.3&q=80&w=1080"
+                className="card-img-top background-img"
+                alt="background-img"
               />
-              <div className="card-img-overlay">
+              <div className="card-img-overlay text-content">
                 <h5 className="card-title">{data.title}</h5>
                 <p className="card-text">
                   <small>Comments</small>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div class="container text-center">
-      <div class="row row-cols-6">
-        <div class="col"><Link to="/">Home</Link></div>
-        <div class="col"><Link to="/Travel">Travel</Link></div>
-        <div class="col"><Link to="/Lifestyle">Lifestyle</Link></div>
-        <div class="col"><Link to="/Business">Business</Link></div>
-        <div class="col"><Link to="/Food">Food</Link></div>
-        <div class="col"><Link to="/Work">Work</Link></div>
-      </div>
+    <div className="container text-center">
+      <p className="slogan-home">&#91; Making your Life Easier &#93;</p>
+      <h1 className="fw-bold"> Discovering the World </h1>
+      <ul class="nav justify-content-center">
+        <li class="nav-item element-nav">
+          <Link to="/">Home</Link>
+        </li>
+        <li class="nav-item element-nav">
+          <Link to="/Travel">Travel</Link>
+        </li>
+        <li class="nav-item element-nav">
+          <Link to="/Lifestyle">Lifestyle</Link>
+        </li>
+        <li class="nav-item element-nav">
+        <Link to="/Business">Business</Link>
+        </li>
+        <li class="nav-item element-nav">
+        <Link to="/Food">Food</Link>
+        </li>
+        <li class="nav-item element-nav">
+        <Link to="/Work">Work</Link>
+        </li>
+      </ul>
     </div>
   );
 };
-
 
 export default Navbar;
